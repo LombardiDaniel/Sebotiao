52f3e352064a440fac339b438fa910849082d2bd
Daniel Lombardi <lombardi.daniel.o@gmail.com>
2021-04-01 17:31:01 -0300

    Added CD script

---
+++ CD.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+IMAGE="lombardi/sebotiao"
+FETCH="git fetch"
+PULL="git pull origin"
+COMPOSE_PATH="/usr/local/bin/docker-compose"
+COMMAND="$COMPOSE_PATH down && $COMPOSE_PATH up --build -d"
+PRJ_PATH=$PWD
+LOG_FILE="CD.log"
+
+logthis() {
+    STR=$1
+    echo "`date '+%d/%m/%Y %H:%M:%S'` - $STR" >> "$LOG_FILE"
+}
+
+cd $PRJ_PATH
+
+latest=$(git pull origin)
+
+if [[ $latest == "Already up to date." ]]; then
+        logthis "Already up to date. Nothing to do."
+else
+        logthis "Update available. Executing update command..."
+        eval $COMMAND
+        logthis "Update completed."
+fi
