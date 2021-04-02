ca7c802af54e425fc49b21e8a1905314b4a00ee8
Daniel Lombardi <lombardi.daniel.o@gmail.com>
2021-04-02 13:00:07 -0300

    creates file beforehand

--- src/utils/audio.py
+++ src/utils/audio.py
@@ -77,6 +77,9 @@ class YoutubeHelper:
         now = datetime.now()
         date_now = now.strftime("%Y/%m/%d/")
 
+        if not os.path.isfile(path):
+            open('/tmp/yt_list.yml', 'w').close()
+
         with open('/tmp/yt_list.yml', 'r+') as yaml_file:
             contents = yaml.load(yaml_file, Loader=yaml.FullLoader)
 
